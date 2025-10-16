af731b54f278817908e4a28fb59b775c67aafc24
Rafael Machado <rafaelmachado2507oliveira@hotmail.com>
2025-10-16 11:33:34 -0300

    upload Execução e manipulação de arquivos
    aulas sobre manipulação de diversos tipos de arquivos em C#

---
+++ Execucao e manipulação de arquivos/Criacao e execucao de arquivos/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace execarquivos
+{
+    class Program
+    {
+
+        static void Executar()
+        {
+
+            var arquivoEntrada = @"C:\Users\Rafae\Downloads\teste.txt";
+            var arquivoSaida = @"C:\Users\Rafae\Downloads\teste_2.txt";
+
+            var linhas = LerArquivo(arquivoEntrada);
+
+            var linhaFiltrada = new List<string>();
+            int i = 0;
+            foreach (var linha in linhas)
+            {
+                if (i < 5)
+                {
+                    linhaFiltrada.Add(linha);
+                }
+                i++;
+            }
+
+            foreach (var linha in linhaFiltrada)
+            {
+                Console.WriteLine(linha);
+            }
+
+            EscreverArquivos(arquivoSaida, linhaFiltrada);
+            AdicionarTexto(arquivoSaida, linhaFiltrada);
+        }
+
+        static string[] LerArquivo(string arquivoEntrada)
+        {
+            try
+            {
+                var linhas = File.ReadAllLines(arquivoEntrada);
+                return linhas;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return new string[] { };
+            }
+        }
+
+        static void EscreverArquivos(string arquivoSaida, List<string> linhas)
+        {
+            try
+            {
+                File.WriteAllLines(arquivoSaida, linhas);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        
+        static void AdicionarTexto(string arquivoSaida, List<string> linhas)
+        {
+            try
+            {
+                File.AppendAllLines(arquivoSaida, linhas);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Executar();
+        }
+    }
+}
+
+// sempre preferir arquivos json para salvar dados
