7e24371d9887597015b4bb135154d61353fa5e2e
Rafael Machado <rafaelmachado2507oliveira@hotmail.com>
2025-10-16 11:32:45 -0300

    upload Encurtador de URL
    criando um encurtador de URL para uso pessoal

---
+++ Encurtador de URL/Encurtador de URL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+namespace encurtaurl
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Dictionary<String, int> urls = new Dictionary<string, int>;
+        }
+
+        /*
+        static void function1()
+        {
+            String urlIncio = "https://learn.microsoft.com";
+            String urlFim = "/en-us/dotnet/csharp/";
+
+            String urlCompleta = urlIncio + urlFim;
+
+            Console.WriteLine("URL completa: " + urlCompleta);
+        }
+        */
+    }
+}
