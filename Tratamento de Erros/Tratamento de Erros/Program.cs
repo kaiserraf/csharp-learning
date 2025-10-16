263d87597870105971a59b89ad4152d204b33e13
Rafael Machado <rafaelmachado2507oliveira@hotmail.com>
2025-10-16 11:36:48 -0300

    upload Tratamento de Erros
    aprendizado de try-catch em C#

---
+++ Tratamento de Erros/Tratamento de Erros/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace dotnetproject
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            int n1, n2, q;
+
+            Console.WriteLine("digite o primeiro número: ");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            try
+            {
+                q = n1 / n2;
+
+                Console.WriteLine($"O quociente da divisão é = {q} ");
+            }
+            catch(System.DivideByZeroException dbz)
+            {
+                Console.WriteLine(dbz);
+            }
+
+        }
+    }
+}
