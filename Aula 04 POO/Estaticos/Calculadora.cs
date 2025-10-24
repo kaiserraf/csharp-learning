1d17ee23763baeff517f99e4df03f2db87a39d2f
Rafael Machado <rafaelmachado2507oliveira@hotmail.com>
2025-10-24 16:46:48 -0300

    Add files via upload

---
+++ Aula 04 POO/Estaticos/Calculadora.cs
@@ -0,0 +1,14 @@
+class Calculadora
+{
+
+    // Atributo numérico
+    public static int numero = 10;
+
+    // metodo para somar dois valores
+
+    public static void soma(int n1, int n2)
+    {
+        Console.WriteLine($"A soma dos valores é {n1 + n2}");
+    }
+
+}
---
+++ Aula 04 POO/Estaticos/Program.cs
@@ -0,0 +1,5 @@
+﻿// exibir o valor contido no atributo numero
+Console.WriteLine($"O valor é: {Calculadora.numero}");
+
+// realizar a soma
+Calculadora.soma(8, 10);
